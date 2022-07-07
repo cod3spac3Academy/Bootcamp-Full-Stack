aaf12d80ce8529e85c4249b1a2705640a33ebe53
demo.proyect <sara255530@gmail.com>
2022-07-07 14:03:26 +0200

    Ej1_apartados_1-5_porTerminar

--- modulo_2/Tema_5_JavaScript/ejercicios/ejercicio1bis/js/mainEj1.js
+++ modulo_2/Tema_5_JavaScript/ejercicios/ejercicio1bis/js/mainEj1.js
@@ -18,6 +18,50 @@ const button1 = document.getElementById("bot1");
 //pruebo con arrow function:
 button1.addEventListener("click", () => document.body.style.backgroundColor = "red");
 
+//Apartado 3:
+//creo las const para los eventos de los enlaces y el de la imagen
+const listItems = document.querySelector("ul");//si le digo por TagName me daba error
+const image = document.querySelector("img");
+//creo la const que hace de función para cambiar el enlace por la imagen:
+const changeImage = (e) => image.src = e.target.textContent;//aquí le digo que la imagen debe ser igual al texto de cada elemento de la lista para luego al llamarlo, que haga esto.Es una arrow function
+
+//ahora le digo que cuando cada elemento sea pinchado, use la función arrow de changeImage:
+listItems.addEventListener("click", changeImage);
+
+//Apartado 4:
+//creo la const para el click del evento:
+const input = document.querySelector("input");//selecionando la propia etqieuta html si no, con id no me salía
+const button2 = document.getElementById("bot2");
+const p3 = document.getElementById("paragraph3");
+
+button2.addEventListener("click", () => {
+    p3.textContent = input.value;//aquí le digo que el contenido del párrafo es lo que haya en la const input
+    input.value = ""; //aquí le digo que la const input es un string
+});
+
+//apartado 4.2: no sé hacerlo
+//Apartado 5:
+const mensaje = document.getElementById("mensaje");
+const contador = document.getElementById("contador");//hago este contador para contar caracteres
+const button3 = document.getElementById("bot3");
+function salirDebajoDelRecuadro(){
+    contador.textContent = mensaje.value;
+};//esta función es para decirle que escriba en el párrafo abajo lo que ponga en recuadro
+mensaje.addEventListener("input", salirDebajoDelRecuadro);//con esto hago que me salga debajo del recuadro
+let minLength = 15;
+mensaje.addEventListener("click", function (e) {
+    if (e.target.value.length > 15){
+       e.target.style.color = "blue";
+
+    }else {
+        e.target.style.color = "red";
+    };
+});
+
+
+
+
+
 
 
 
